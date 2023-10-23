d168481e7b77d6cee06faf573b19b26e9ef71e69
Javier Ruiz <javierruizjr15@gmail.com>
2023-10-23 12:29:27 -0700

    test5 pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,15 @@
 const repeatString = function(val, num) {
+    console.log(num);
+    if ( num < 0 ) {
+        return "ERROR"
+    }
+
     const result = [];
     for ( i =0; i<num; i++) {
         result.push(val);
     }
     const resultString = result.join('');
-    return resultString;
+        return resultString;
 };
 
 // Do not edit below this line
