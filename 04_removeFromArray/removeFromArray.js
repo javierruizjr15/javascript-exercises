c2afd3cec97e64bd27994ed3126eb7490609b016
Javier Ruiz <javierruizjr15@gmail.com>
2023-10-23 15:15:16 -0700

    test1 pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, numRMV) {
+    const newArray = [];
+    for ( i=0; i<arr.length; i++) {
+        if (arr[i]!==numRMV) {
+            newArray.push(arr[i]);
+        }
+    }
+    console.log(newArray);
+    console.log(numRMV);
+    return newArray;
 };
 
 // Do not edit below this line
