cb8fafc02adc838ff191ee295b16815252dff644
Javier Ruiz <javierruizjr15@gmail.com>
2023-10-23 14:52:32 -0700

    test2 pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(val) {
+    var reversed = '';
+    for ( i=val.length - 1; i>=0; i--) {
+        reversed += val[i];
+    }
+    return reversed; 
 };
 
 // Do not edit below this line
