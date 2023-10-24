5e263cccf3e49ecebd642feefcca93069382b7bf
Javier Ruiz <javierruizjr15@gmail.com>
2023-10-24 08:33:02 -0700

    test3 pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,15 @@
 const sumAll = function(num1, num2) {
     var finalSum = 0;
     console.log(finalSum);
-       for (i=num1; i<=num2; i++) {
+       if (num1 < num2) {
+        for (i=num1; i<=num2; i++) {
         finalSum += i;
        }
+    } else if (num1 > num2) {
+        for (i=num1; i>=num2; i--) {
+            finalSum += i;
+        }
+    }
     console.log(finalSum);
     return finalSum;
 };
