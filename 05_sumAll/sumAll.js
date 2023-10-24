c7e4dcf09f37b32d25abafbdbbd5bf357365a9b3
Javier Ruiz <javierruizjr15@gmail.com>
2023-10-24 09:22:56 -0700

    simplified

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,17 +2,18 @@ const sumAll = function (num1, num2) {
   var finalSum = 0;
   if (num1 < 0 || num2 < 0) return "ERROR";
   if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR"
+  if (num1 > num2) {
+    const temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
 
   console.log(finalSum);
   if (num1 < num2) {
     for (i = num1; i <= num2; i++) {
       finalSum += i;
     }
-  } else if (num1 > num2) {
-    for (i = num1; i >= num2; i--) {
-      finalSum += i;
-    }
-  }
+  } 
   console.log(finalSum);
   return finalSum;
 };
