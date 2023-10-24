12471d30c1e324a9209dc8082febbd58d2bfd5db
Javier Ruiz <javierruizjr15@gmail.com>
2023-10-24 09:01:47 -0700

    test5 pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,24 @@
-const sumAll = function(num1, num2) {
-    var finalSum = 0;
-    console.log(finalSum);
-       if (num1 < num2) {
-        for (i=num1; i<=num2; i++) {
-        finalSum += i;
-       }
-    } else if (num1 > num2) {
-        for (i=num1; i>=num2; i--) {
-            finalSum += i;
-        }
+const sumAll = function (num1, num2) {
+  var finalSum = 0;
+
+  console.log(finalSum);
+  if (num1 < num2) {
+    for (i = num1; i <= num2; i++) {
+      finalSum += i;
     }
-    console.log(finalSum);
-    return finalSum;
+  } else if (num1 > num2) {
+    for (i = num1; i >= num2; i--) {
+      finalSum += i;
+    }
+  }
+  console.log(finalSum);
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR"
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
