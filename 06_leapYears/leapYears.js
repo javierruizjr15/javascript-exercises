b1a3397414747234b57d02655ec3807053bf678c
Javier Ruiz <javierruizjr15@gmail.com>
2023-10-24 10:08:10 -0700

    simplified pass all

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,15 @@
+// const leapYears = function(year) {
+//     if (year % 4 == 0 && year % 100 !== 0) { 
+//         return true;
+//     } else if (year % 100 == 0 && year % 400 == 0) { 
+//          return true;
+//     } else {
+//         return false;
+//     }
+// };
+
 const leapYears = function(year) {
-    if (year % 4 == 0 && year % 100 !== 0) { 
-        return true;
-    } else if (year % 100 == 0 && year % 400 == 0) { 
-         return true;
-    } else {
-        return false;
-    }
+    return year % 4 == 0 && (year % 100 !== 0 || year % 400 == 0)
 };
 
 // Do not edit below this line
