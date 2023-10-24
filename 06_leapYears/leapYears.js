60ab6512edabbdd7b6bc51483be3017e42ea6941
Javier Ruiz <javierruizjr15@gmail.com>
2023-10-24 09:54:33 -0700

    test2 pass

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
 const leapYears = function(year) {
-    if (year % 4 == 0) return true;
-    if (year % 100 == 0 && year % 400 != 0) return false;
+    if (year % 4 == 0) { 
+        return true;
+    } else if (year % 100 == 0 && year % 400 != 0) { 
+         return false;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
