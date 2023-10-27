57e049bb4a6f0b2f487b82f4526dcafbb6e11b22
Javier Ruiz <javierruizjr15@gmail.com>
2023-10-27 10:06:49 -0600

    test1 pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * 5/9;
+  console.log("c here: " + celsius)
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = (celsius * 9/5) + 32;
+  console.log("f here: " + fahrenheit);
+  return fahrenheit;
 };
 
 // Do not edit below this line
